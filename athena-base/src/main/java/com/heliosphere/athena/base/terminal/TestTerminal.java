b8cc405840dd8c7436e3513fb1d0e7f81fa611c8
Resse Christophe <christophe.resse@gmail.com>
2017-06-08 18:20:11 +0200

    Implemented terminal entities.

---
+++ athena-base/src/main/java/com/heliosphere/athena/base/terminal/TestTerminal.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright(c) 2017 - Heliosphere Corp.
+ * ---------------------------------------------------------------------------
+ * This file is part of the Heliosphere's project which is licensed under the 
+ * Apache license version 2 and use is subject to license terms.
+ * You should have received a copy of the license with the project's artifact
+ * binaries and/or sources.
+ * 
+ * License can be consulted at http://www.apache.org/licenses/LICENSE-2.0
+ * ---------------------------------------------------------------------------
+ */
+package com.heliosphere.athena.base.terminal;
+
+import com.heliosphere.athena.base.file.internal.FileException;
+
+public class TestTerminal
+{
+	/**
+	 * Main entry point of the application.
+	 * <hr>
+	 * @param arguments Arguments entered on the command line.
+	 */
+	@SuppressWarnings("nls")
+	public static void main(String[] arguments)
+	{
+		try
+		{
+			Terminal terminal = new Terminal("/config/command/chat-commands.xml");
+			terminal.start();
+		}
+		catch (FileException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+}
